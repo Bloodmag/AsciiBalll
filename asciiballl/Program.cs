9a8b98e83ec47d493dbc5d66bb910451ac26dc91
Данила Золотопупов <bloodmagv@gmail.com>
2019-07-23 17:06:16 +0300

    new render

--- asciiballl/Program.cs
+++ asciiballl/Program.cs
@@ -1,30 +1,163 @@
 ﻿using System;
+using System.Threading;
 using System.Timers;
+using ConsoleWindowRenderer;
 
 namespace asciiballl
 {
     class Program
     {
+        static string[] map = new string[13] { "#*#*#*#*",
+                                        "*000000#",
+                                        "#000000*",
+                                        "*000000#",
+                                        "#000000*",
+                                        "*000000#",
+                                        "#000000*",
+                                        "*000000#",
+                                        "#000000*",
+                                        "*000000#",
+                                        "#000000*",
+                                        "*000000#",
+                                        "#*#*#*#*"};
+
+        static int mapHeight = 13;
+        static int mapWidth = 8;
+
+        static char[] brush = new char[] { '\u2588' ,'\u2593', '\u2592', '\u2591' };
+        static char[] brush2 = " .:-=+*#%@\u2588".ToCharArray();
+
+        static int viewHeight = 40;
+        static int viewWidth = 120;
+
+        static char[] frame = new string(' ', viewWidth * viewHeight).ToCharArray();
+
         static void Main(string[] args)
         {
-            Console.SetWindowSize(150, 60);
-            string s = "";
-            var timer = DateTime.Now;
-            long a = 0;
-            while (DateTime.Now.Subtract(timer).TotalMilliseconds <= 10000)
+            W.SetupBuffer(viewWidth, viewHeight);
+            Console.CursorVisible = false;
+
+            Vector2d pos = new Vector2d(2, 2);
+            Vector2d dir = new Vector2d(1, 0);
+            while (true)
             {
-                s = "";
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.SetCursorPosition(0, 0);
-                for (int i = 0; i < 150 * 6; i++) s += "0000000000";
-                for (int i = 0; i < 150 * 60; i++)
-                Console.Write(s[i]);
-                a++;
+                //Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                for(int i = 0; i < viewWidth; i++)
+                {
+                    var h = GetHeights(pos, dir, Math.PI * (i - viewWidth / 2)/viewWidth / 4);
+                    for(int j = 0; j<viewHeight; j++)
+                    {
+                        if(j>=h.Item1&&j<h.Item2)
+                        //frame[j * viewWidth + i] = brush[h.Item3];
+                        {
+                            //Console.ForegroundColor = (h.Item4) ? ConsoleColor.Red : ConsoleColor.White;
+                            //Console.SetCursorPosition(i, j);
+                            //Console.Write(brush[h.Item3]);
+                            frame[j * viewWidth + i] = h.Item4? brush[0]:brush[1];
+                        }
+                        else
+                        {
+                            //Console.Write(' ');
+                            frame[j * viewWidth + i] = ' ';
+                        }
+                            
+                    }
+                }
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.LeftArrow) dir.Rotate(-0.1);
+                    if (key.Key == ConsoleKey.RightArrow) dir.Rotate(0.1);
+                    if (key.Key == ConsoleKey.UpArrow) pos+=dir/10;
+                }
+                for(int i = 0; i< viewHeight * viewWidth;i++)
+                {
+                    W.Buf[i].Char.AsciiChar = (byte)frame[i];
+                    W.Buf[i].Attributes = 3;
+                }
+                W.Draw();
+                //Console.SetCursorPosition(0, 0);
+                //Console.Write(frame);
+                //Console.SetCursorPosition(0, 0);
+                //Console.Write("dir.X = {0}  dir.y = {1}   {2}",dir.X.ToString(),dir.Y.ToString(),brush[0]);
+                Thread.Sleep(50);
             }
-            Console.Clear();
-            Console.WriteLine(a);
-            Console.ReadLine();
+
         }
+
+        static (int,int,int, bool) GetHeights(Vector2d pos, Vector2d dir, double angle)
+        {
+            Vector2d newdir = new Vector2d(dir.X, dir.Y);
+            newdir.Rotate(angle);
+            double distanceToWall = 0;
+            double maxDistance = 16;
+            bool hit = false;
+            char e = ' ';
+            while (!hit && distanceToWall < maxDistance)
+            {
+                distanceToWall += 0.1;
+                e = map[(int)Math.Round(pos.Y + newdir.Y * distanceToWall)][(int)Math.Round(pos.X + newdir.X * distanceToWall)];
+                if ( e == '#'|| e=='*') hit = true;
+            }
+            int bot = viewHeight - (int)Math.Round(viewHeight / 2 - (viewHeight / distanceToWall));
+            int top = viewHeight / 2 ;
+            return (top, bot,(int)Math.Round(10 * (distanceToWall / maxDistance)),(e=='#'));
+        }
+    }
+
+    public static class Renderer
+    {
+        static int viewHeight = 40;
+        static int viewWidth = 120;
+    }
+    public class Vector2d
+    {
+        private double x;
+        private double y;
+
+        public Vector2d(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public double Length { get => Math.Sqrt(x * x + y * y); }
+
+        public void Normalize()
+        {
+            double l = Length;
+            x /= l;
+            y /= l;
+        }
+
+        public void Rotate(double angle)
+        {
+            double tmp = x * Math.Cos(angle) - y * Math.Sin(angle);
+            y = x * Math.Sin(angle) + y * Math.Cos(angle);
+            x = tmp;
+        }
+
+        public static Vector2d operator - (Vector2d v)
+        {
+            return new Vector2d(-v.x, -v.y);
+        }
+
+        public static Vector2d operator - (Vector2d l, Vector2d r)
+        {
+            return new Vector2d(l.x-r.x,l.y-r.y);
+        }
+
+        public static Vector2d operator +(Vector2d l, Vector2d r)
+        {
+            return new Vector2d(l.x + r.x, l.y + r.y);
+        }
+        public static Vector2d operator /(Vector2d l, double r)
+        {
+            return new Vector2d(l.X/r, l.Y / r);
+        }
+
     }
 }
---
+++ asciiballl/W.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace ConsoleWindowRenderer
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Coord
+    {
+        public short X;
+        public short Y;
+
+        public Coord(short X, short Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    };
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct CharUnion
+    {
+        [FieldOffset(0)] public char UnicodeChar;
+        [FieldOffset(0)] public byte AsciiChar;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct CharInfo
+    {
+        [FieldOffset(0)] public CharUnion Char;
+        [FieldOffset(2)] public short Attributes;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SmallRect
+    {
+        public short Left;
+        public short Top;
+        public short Right;
+        public short Bottom;
+    }
+    static class W
+    {
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern SafeFileHandle CreateFile(
+            string fileName,
+            [MarshalAs(UnmanagedType.U4)] uint fileAccess,
+            [MarshalAs(UnmanagedType.U4)] uint fileShare,
+            IntPtr securityAttributes,
+            [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+            [MarshalAs(UnmanagedType.U4)] int flags,
+            IntPtr template);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteConsoleOutput(
+          SafeFileHandle hConsoleOutput,
+          CharInfo[] lpBuffer,
+          Coord dwBufferSize,
+          Coord dwBufferCoord,
+          ref SmallRect lpWriteRegion);
+
+        
+
+        private static readonly SafeFileHandle h;
+        private static CharInfo[] buf;
+        private static SmallRect rect;// new SmallRect() { Left = 0, Top = 0, Right = 120, Bottom = 40 };
+        private static int width;
+        private static int height;
+
+        public static CharInfo[] Buf { get => buf; }
+
+        static W()
+        {
+            h = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+            if (h.IsInvalid)
+            {
+                throw new ExternalException("Unable to get console buffer access!");
+            }
+        }
+
+        public static bool SetupBuffer(int _width, int _height)
+        {
+            width = _width;
+            height = _height;
+            rect = new SmallRect() { Left = 0, Top = 0, Right = (short)width, Bottom = (short)height };
+            buf = new CharInfo[width*height];
+            Console.SetWindowSize(width, height);
+            return true;
+        }
+
+        public static bool Draw()
+        {
+            return WriteConsoleOutput(h, Buf,
+                              new Coord() { X = (short)width, Y = (short)height },
+                              new Coord() { X = 0, Y = 0 },
+                              ref rect);
+        }
+
+    }
+}
+
